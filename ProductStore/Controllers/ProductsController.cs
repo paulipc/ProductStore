c41f9e6e6a575f114c8aea2e3e0bb51ad9b9e09d
Pauli Sutelainen <pauli.sutelainen@gmail.com>
2017-02-06 17:17:11 +0200

    First commit

---
+++ ProductStore/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductStore.Models;
+
+namespace ProductStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+    }
+}
